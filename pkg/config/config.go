481b8660e9a8e7a61473f4104afab20518373809
Zech-Hein <31706984+Zech-Hein@users.noreply.github.com>
2019-10-01 13:08:27 -0500

    Update config.go

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -13,12 +13,12 @@ import (
 	"encoding/json"
 	"os"
 
-	config "github.com/micro/go-micro/config"
+	config "github.com/micro/go-micro"
 	"github.com/micro/go-micro/config/source/env"
 	"github.com/micro/go-micro/config/source/file"
 	"github.com/micro/go-micro/config/source/flag"
 	"github.com/micro/go-micro/config/source/memory"
-	"github.com/redhat-cop/cert-operator/pkg/certs"
+	"github.com/Zech-Hein/cert-operator/pkg/certs"
 	"github.com/sirupsen/logrus"
 )
 
